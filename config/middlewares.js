97a71e8e9a5dae0e70b1a8eb7f5639022d986cdd
nKeys Kuo <nkeyskuo124@gmail.com>
2024-05-24 09:27:18 +0700

    Limit origin cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,23 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: [
+        "https://xuongkhopvietducbienhoa.com",
+        "https://xuongkhopbienhoa.com",
+        "https://vietducbienhoa.com",
+      ], // Thay thế với các domain của bạn
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
